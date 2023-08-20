7f18d3049cb3c25aa0e020cf57e7ef61a3c1e87e
A91y <ayush.agr254@gmail.com>
2023-08-20 20:40:01 +0530

    Python program to convert Fahrenheit  to Celsius

---
+++ 4 Fahrenheit to Celsius.py
@@ -0,0 +1,5 @@
+# Write a Python program to convert Fahrenheit temperature to Celsius temperature.
+
+celcius : float = float(input("Enter the temperature in celcius: "))
+fahrenheit : float = (celcius * 9/5) + 32
+print(f"{celcius} celcius is equal to {fahrenheit} fahrenheit")
