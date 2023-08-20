66a125f260c5cdccdc9315681e073dfdd67732eb
A91y <ayush.agr254@gmail.com>
2023-08-20 21:25:49 +0530

    smallest in three

---
+++ 14 Smallest in three.py
@@ -0,0 +1,13 @@
+# Write a C program to find the smallest number between the three numbers using different methods.
+
+a : int = int(input("Enter first number: "))
+b : int = int(input("Enter second number: "))
+c : int = int(input("Enter third number: "))
+
+print(a if a<c else c if a<b else b if b<c else c, "is smaller by Method 1")
+
+print(((c, b)[b<c], (c, a)[a<c])[a<b], "is smaller by Method 2 (Tuple Method)")
+
+print({True: {True: a, False: c}[a<c], False: {True: b, False: c}[b<c]}[a<b], "is smaller by Method 3 (Dictionary Method)")
+
+print((lambda: (lambda: c, lambda: b)[b<c](), lambda: (lambda: c, lambda: a)[a<c]())[a<b](), "is smaller by Method 4 (Lambda Method)")
