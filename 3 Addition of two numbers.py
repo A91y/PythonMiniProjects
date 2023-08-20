7c145388e704ce879684bd18a5d1f9579d8bd0bf
A91y <ayush.agr254@gmail.com>
2023-08-20 20:37:43 +0530

    Python program to find addition of two numbers

---
+++ 3 Addition of two numbers.py
@@ -0,0 +1,6 @@
+# Write a Python program to find addition of two numbers
+
+num1 : float = float(input("Enter the first number: "))
+num2 : float = float(input("Enter the second number: "))
+addition : float = num1 + num2
+print(f"Addition of {num1} and {num2} is {addition}")
