044746b77593813283b3a8eb6cba1da288b67653
A91y <ayush.agr254@gmail.com>
2023-08-20 21:13:26 +0530

    largest number

---
+++ 11 Largest in two.py
@@ -0,0 +1,12 @@
+# Write a C program to find the largest number between the two numbers using different methods.
+
+a : int = int(input("Enter first number: "))
+b : int = int(input("Enter second number: "))
+
+print(a if a>b else b, "is larger by Method 1")
+
+print((b, a)[a>b], "is larger by Method 2 (Tuple Method)")
+
+print({True: a, False: b}[a>b], "is larger by Method 3 (Dictionary Method)")
+
+print((lambda: a, lambda: b)[a>b], "is larger by Method 4 (Lambda Method)")
