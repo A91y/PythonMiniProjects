43178824061b5aa98cb5662bf0c6126a678a5404
A91y <ayush.agr254@gmail.com>
2023-08-21 21:49:42 +0530

    check whether two numbers are equal or not

---
+++ 19 Equal or not.py
@@ -0,0 +1,6 @@
+# Write a Python program to check whether two numbers are equal or not.
+
+a : int = int(input("Enter first number: "))
+b : int = int(input("Enter second number: "))
+
+print("Equal" if a==b else "Not equal")
