10c224884da5cd28669b18e090b7b3dbff7dd674
A91y <ayush.agr254@gmail.com>
2023-08-21 22:01:37 +0530

    positive or negative or zero

---
+++ 22 Positive or negative or zero.py
@@ -0,0 +1,10 @@
+# Write a Python program to check that a number is positive or negative or zero.
+
+a : int = int(input("Enter a number: "))
+
+if a>0:
+    print("Positive")
+elif a<0:
+    print("Negative")
+else:
+    print("Zero")
