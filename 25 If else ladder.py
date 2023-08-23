8c3c87c8be5aea9232e0fbe2c827fe0da63e8f2f
A91y <ayush.agr254@gmail.com>
2023-08-23 23:18:58 +0530

    y using if-else ladder

---
+++ 25 If else ladder.py
@@ -0,0 +1,17 @@
+# Write a C program to find the value of y using if-else ladder.
+#         1+x     n=1
+#  y =    1+x/n   n=2
+#         1+x^n    n=3
+#         1+nx    otherwise
+
+n: int = int(input("Enter n: "))
+x: int = int(input("Enter x: "))
+if n == 1:
+    y = 1 + x
+elif n == 2:
+    y = 1 + x/n
+elif n == 3:
+    y = 1 + x**n
+else:
+    y = 1 + n*x
+print("y = ", y)
